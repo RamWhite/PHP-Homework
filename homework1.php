4fbe089fd871822fc030db01f5298031f72ddc23
Kipshakbaev Meiram <ramwhite@mail.ru>
2019-02-09 21:29:09 +0600

    finish

--- homework1.php
+++ homework1.php
@@ -9,42 +9,99 @@
 </head>
 <body>
     <div>
-    <h1>Задание №1</h1>
+    <h1>Задание №1 (готово)</h1>
         <?php
             $array = array();
-            $len = 10;
-            $min = -10;
-            $max = 10;
-            for ($i = 0; $i < $len; $i++){
-                $array[] = mt_rand($min, $max);
+            for ($i = 0; $i < 10; $i++){
+                $array[] = mt_rand(-10, 10);
             }
-            var_dump ($array);
+
+            //Без min и max
+            echo "<h3>Методом перебора</h3>";
+            $min = $array[0];
+            foreach ($array as $key){
+                if ($key < $min){
+                    $min = $key;
+                }
+            }
+            $my_min_weight = array_keys($array, $min)[0];
+            echo "<br>Индекс min числа: " , $my_min_weight , "<br>Минимальный элемент: " , $min;;
+
+            $max = $array[0];
+            foreach ($array as $key){
+                if ($key > $max){
+                    $max = $key;
+                }
+            }
+            $my_max_weight = array_keys($array, $max)[0];
+            echo "<br>Индекс max числа: " , $my_max_weight , "<br>Максимальный элемент: " , $max;
+
+            //С использованием min и max
+            echo "<h3>Через функции</h3>";
+            $min_weight = array_keys($array, min($array))[0];
+            $max_weight = array_keys($array, max($array))[0];
+            echo "<br>" , "Индекс min числа: " , $min_weight, "<br>" , "Минимальный элемент: " , min($array);
+            echo "<br>" , "Индекс max числа: " , $max_weight, "<br>" , "Максимальный элемент: " , max($array);
         ?>
     </div>
 
     <div>
-    <h1>Задание №2</h1>
+    <h1>Задание №2 (готово)</h1>
         <?php
-            
+            $array = array();
+            for ($i = 0; $i < 10; $i++){
+                $array[] = mt_rand(-10, 10);
+            }
+
+            $l = 1;
+            $count = 0;
+            foreach ($array as $key){
+                if (($key % 3 == 0 || $key % 2 == 0) && $key % 6 != 0){
+                    echo $key , "; ";
+                    $count++;
+                    $l *= $key;
+                }
+            }
+            echo "<br>Количество: " , $count;
+            echo "<br>Произведение: " , $l;
         ?>
     </div>
 
     <div>
     <h1>Задание №3</h1>
         <?php
-            
+            $array = array();
+            for ($i = 0; $i < 10; $i++){
+                $array[] = mt_rand(-10, 10);
+            }
+
+            print_r ($array);
+
+            $s = 0;
+            foreach (array_keys($array) as $key){
+                if (pow($key, 2) > 16){
+                    $s += $key;
+                }
+            }
+            echo "<br>Сумма индексов: " , $s;
         ?>
     </div>
 
     <div>
-    <h1>Задание №4</h1>
+    <h1>Задание №4 (готово)</h1>
         <?php
-            
+            $array = array();
+            for ($i = 0; $i < 3; $i++){
+                for ($j = 0; $j < 3; $j++)
+                    $array[] = mt_rand(-10, 10);
+            }
+
+            print_r ($array);
         ?>
     </div>
 
     <div class = "table">
-    <h1>Задание №5 - Таблица умножения</h1>
+    <h1>Задание №5 - Таблица умножения (готово)</h1>
         <table>
             <?php
                 for ($i = 1; $i <= 10; $i++){
@@ -59,9 +116,43 @@
     </div>
 
     <div>
-    <h1>Дополнительное задание</h1>
+    <h1>Дополнительное задание (готово)</h1>
         <?php
-            
+            $array = array();
+            for ($i = 0; $i < 3; $i++){
+                for ($j = 0; $j < 3; $j++)
+                    $array[] = mt_rand(-10, 10);
+            }
+
+            //Без min и max
+            echo "<h3>Методом перебора</h3>";
+            $min = $array[0];
+            foreach ($array as $key){
+                if ($key < $min){
+                    $min = $key;
+                }
+            }
+            $my_min_weight = array_keys($array, $min)[0];
+            echo "<br>Индекс min числа: " , $my_min_weight;
+            echo "<br>Минимальный элемент: " , $min;
+
+            $max = $array[0];
+            foreach ($array as $key){
+                if ($key > $max){
+                    $max = $key;
+                }
+            }
+            $my_max_weight = array_keys($array, $max)[0];
+            echo "<br>Индекс max числа: " , $my_max_weight;
+            echo "<br>Максимальный элемент: " , $max;
+
+
+            //С использованием min и max
+            echo "<h3>Через функции</h3>";
+            $min_weight = array_keys($array, min($array))[0];
+            $max_weight = array_keys($array, max($array))[0];
+            echo "<br>" , "Индекс min числа: " , $min_weight, "<br>" , "Минимальный элемент: " , min($array);
+            echo "<br>" , "Индекс max числа: " , $max_weight, "<br>" , "Максимальный элемент: " , max($array);
         ?>
     </div>
 </body>
